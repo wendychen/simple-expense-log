02425dde472ef7371cb3818956e5c3587d3a7304
Wendy Chen <lfwendy@gmail.com>
2026-01-31 20:14:19 +0800

    Updated ExpenseList.tsx

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -168,7 +168,7 @@ const ExpenseList = ({
             <div className="space-y-2">
               {dayExpenses.map((expense, index) => (
                 const categoryKey = (expense.category || "misc") as ExpenseCategory;
-                const categoryMeta = EXPENSE_CATEGORIES[categoryKey];
+                const categoryMeta = ExpenseCategory[categoryKey];
                 return (
               
                 <div
